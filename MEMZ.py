d94032b5661875c1ae7a5bcab18628c25f19362a
Samidy <168582143+SamidyFR@users.noreply.github.com>
2024-09-18 12:06:11 +0300

    Update MEMZ.py
    fixed.

--- MEMZ.py
+++ MEMZ.py
@@ -3,7 +3,7 @@ import time
 import random
 import webbrowser
 
-sites = ["https://www.google.co.ck/search?q=how+2+remove+a+virus" "https://www.google.co.ck/search?q=how+to+get+money" "https://play.clubpenguin.com" "https://www.google.co.ck/search?q=batch+virus+download" "https://www.google.co.ck/search?q=what+happens+if+you+delete+system32" "https://www.google.co.ck/search?q=bonzi+buddy+download+free" "https://www.google.co.ck/search?q=how+2+buy+weed" "https://www.google.co.ck/search?q=my+computer+is+doing+weird+things+wtf+is+happenin+plz+halp" "https://www.pcoptimizerpro.com" "https://www.google.co.ck/search?q=mcafee+vs+norton" "https://www.google.co.ck/search?q=internet+explorer+is+the+best+browser"]
+sites = ["https://www.google.co.ck/search?q=how+2+remove+a+virus", "https://www.google.co.ck/search?q=how+to+get+money", "https://play.clubpenguin.com", "https://www.google.co.ck/search?q=batch+virus+download", "https://www.google.co.ck/search?q=what+happens+if+you+delete+system32", "https://www.google.co.ck/search?q=bonzi+buddy+download+free", "https://www.google.co.ck/search?q=how+2+buy+weed", "https://www.google.co.ck/search?q=my+computer+is+doing+weird+things+wtf+is+happenin+plz+halp", "https://www.pcoptimizerpro.com", "https://www.google.co.ck/search?q=mcafee+vs+norton", "https://www.google.co.ck/search?q=internet+explorer+is+the+best+browser"]
 
 
 run = True
